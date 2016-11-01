c08b69be1ab4cf8e1130021baab64dbec0f72da1
Hristo Deshev <hristo@deshev.com>
2016-11-01 17:10:27 +0200

    Upgrade to latest Appium

--- ci-android.sh
+++ ci-android.sh
@@ -30,7 +30,7 @@ build_app() {
 test_app() {
     APP="$1"
     (cd "$APP" && \
-        npm run appium-android-only)
+        npm run test-appium-android)
 }
 
 for app in {test-ng,test-ts,test-js} ; do
