1bde7b36eb1a8bf894eb364c35159a8c12952ba9
Hristo Deshev <hristo@deshev.com>
2017-01-11 14:47:11 +0200

    Increase wait for button timeout.

--- test-js/e2e-tests/sample-test.js
+++ test-js/e2e-tests/sample-test.js
@@ -19,7 +19,7 @@ describe("button tap", function () {
 
     it("should find an element", function () {
         return driver
-            .waitForElementByAccessibilityId("tapButton", 100000)
+            .waitForElementByAccessibilityId("tapButton", 300000)
             .elementByAccessibilityId("tapButton")
                 .should.eventually.exist
             .click()
